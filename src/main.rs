72baa0edbb9ab6eda5fb9b17b0e964e51b4d7db4
Paweł Zmarzły <pawo2500@gmail.com>
2018-10-28 15:31:03 +0100

    add techniques used in x11-input-supercharger

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() {
         return;
     }
 
+    // Consider using asset_str from `static_assets` crate, so your
+    // crate won't recompile in debug mode after an XML change.
+    // As of static_assets 0.1.1:
+    // let glade_src = &asset_str!("src/test.glade");
     let glade_src = include_str!("test.glade");
     let builder = gtk::Builder::new_from_string(glade_src);
 
@@ -32,5 +36,19 @@ fn main() {
     gtk::StyleContext::add_provider_for_screen(&screen, &style, gtk::STYLE_PROVIDER_PRIORITY_USER);
 
     window.show_all();
+
+    // Gives GTK control over program execution:
     gtk::main();
+    // Alternatively, to have Rust code control your program and its GTK+ parts, use:
+    // loop {
+    //     // do some work here, e.g.
+    //     while let Ok(ev) = rx.try_recv() {
+    //         // ...
+    //     }
+    //     // then:
+    //     gtk::main_iteration_do(false);
+    //     sleep_ms(4); // May be unnecessary in your use case.
+    //                  // 4ms for consistency - some OS round values to multiplies of 4
+    //                  // with e.g. 1ms turned into 4ms
+    // }
 }
